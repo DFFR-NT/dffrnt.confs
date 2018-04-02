718dd366d45e37c4e76fa0efcc22f45806ec9031
Arian Johnson <leshaun.john@gmail.com>
2018-04-01 19:54:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // ----------------------------------------------------------------------------------------------
 // Handle Requires ------------------------------------------------------------------------------
 
-	import { ROOTD } from 'dffrnt.utils';
+	const { ROOTD } = require('dffrnt.utils');
 	const pth = `${ROOTD}/config`;
 
-	export const Settings 	= require(`${pth}/settings.cfg`).default;
-	export const DB 		= require(`${pth}/database.cfg`).default;
-	export const NMSP 		= require(`${pth}/namespaces.cfg`).default;
-	export const AuthP 		= require(`${pth}/authpoints.cfg`).default;
-	export const  EndP 		= require(`${pth}/endpoints.cfg`).default;
-
-	export default { Settings, DB, NMSP, AuthP, EndP };
+	module.exports 	= {
+		Settings: 	require(`${pth}/settings.cfg`),
+		DB: 		require(`${pth}/database.cfg`),
+		NMSP: 		require(`${pth}/namespaces.cfg`),
+		AuthP: 		require(`${pth}/authpoints.cfg`),
+		EndP: 		require(`${pth}/endpoints.cfg`)
+	};

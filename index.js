2175e20c0ed3405e8956dd9b59cd71e79bef6d16
LeShaunJ <leshaun.john@gmail.com>
2019-02-10 17:19:35 -0700

    Made configuration more streamlined, complete with documentation

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
+/**
+ * A configuration module for the `dffrnt.api` framework
+ * @module dffrnt.confs
+ * @alias confs
+ */
+
 
 // ----------------------------------------------------------------------------------------------
-// Handle Requires ------------------------------------------------------------------------------
+// // Handle Requires ------------------------------------------------------------------------------
 
 	const ROOTD = require('app-root-path');
 	const pth 	= `${ROOTD}/config`;
 
 	module.exports 	= {
-		Settings: 	require(`${pth}/settings.cfg`),
-		DB: 		require(`${pth}/database.cfg`),
-		NMSP: 		require(`${pth}/namespaces.cfg`),
-		AuthP: 		require(`${pth}/authpoints.cfg`),
-		EndP: 		require(`${pth}/endpoints.cfg`)
-	};
+		/**
+		 * Initializes all available Configurators
+		 * @alias module:confs.Definers
+		 */
+		Definers()	{ 
+			let { RouteAU, RouteDB, GNHeaders, GNParam, GNDescr,
+			      PType, PT, _Defaults, MERGER 
+			    } = require('./lib/utils'); 
+			return { 
+				RouteAU, RouteDB, GNHeaders, GNParam, GNDescr, 
+				PType, PT, _Defaults, MERGER 
+			};
+		},
+
+		/**
+		 * Initializes all customized Configurators
+		 * @alias module:confs.Init
+		 */
+		Init() 		{ 
+			return {
+				Settings: 	require(`${pth}/settings.cfg`),
+				DB: 		require(`${pth}/database.cfg`),
+				NMSP: 		require(`${pth}/namespaces.cfg`),
+				AuthP: 		require(`${pth}/authpoints.cfg`),
+				EndP: 		require(`${pth}/endpoints.cfg`),
+			};	
+		},
+	}
+
+

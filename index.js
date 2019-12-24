cad61de4df8e44164ee9206bd0f2c3e7ac612691
LeShaunJ <leshaun.john@gmail.com>
2019-12-23 20:38:10 -0700

    Upped to dffrnt.api v4.0.0

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-/**
- * A configuration module for the `dffrnt.api` framework
- * @module dffrnt.confs
- * @alias confs
- */
-
+/// <reference path="index.d.ts" />
 
 // ----------------------------------------------------------------------------------------------
 // // Handle Requires ------------------------------------------------------------------------------
 
 	const ROOTD = require('app-root-path');
 	const pth 	= `${ROOTD}/config`;
+	const Definers = require('./lib/utils');
 
 	module.exports 	= {
-		/**
-		 * Initializes all available Configurators
-		 * @alias module:confs.Definers
-		 */
-		Definers()	{ 
-			let { RouteAU, RouteDB, GNHeaders, GNParam, GNDescr,
-			      PType, PT, _Defaults, MERGER 
-			    } = require('./lib/utils'); 
-			return { 
-				RouteAU, RouteDB, GNHeaders, GNParam, GNDescr, 
-				PType, PT, _Defaults, MERGER 
-			};
-		},
+		...Definers,
 
-		/**
-		 * Initializes all customized Configurators
-		 * @alias module:confs.Init
-		 */
 		Init() 		{ 
 			return {
 				Settings: 	require(`${pth}/settings.cfg`),
